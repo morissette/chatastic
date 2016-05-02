3c5597146f4d2e30a9fd69b7ab8a8417f3d896ee
Matthew Harris <mharris@doublehorn.com>
2016-05-02 17:45:19 -0500

    Setup notification endpoint

--- backend/application.py
+++ backend/application.py
@@ -4,6 +4,7 @@ from flask.ext.cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from boto3.session import Session
 
 # Setup Core Flask
 app = Flask(__name__)
@@ -43,10 +44,30 @@ class HipChat(Resource):
     def post():
         pass
 
+class Notification(Resource):
+
+    def post(self):
+        """
+        create notification in SQS
+        takes JSON
+        {
+            "id": \d+,
+            "account_id": \d+,
+            "name": \S+,
+            "provider": \d+,
+            "message": \d+,
+        }
+        """
+
+        pass
+
 # Setup Routes
 api.add_resource(Slack, '/slack')
 api.add_resource(PagerDuty, '/pagerduty')
 api.add_resource(HipChat, '/hipchat')
+
+# Notification Routes
+api.add_resource(Notification, '/notification')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
