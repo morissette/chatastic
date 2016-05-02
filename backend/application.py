e78c6eead5f77b20ffbc64bc1d60ae4adfc2b11a
Matthew Harris <mharris@doublehorn.com>
2016-05-02 17:05:40 -0500

    Added db migration code

--- backend/application.py
+++ backend/application.py
@@ -1,10 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask.ext.cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
+# Setup Core Flask
 app = Flask(__name__)
 CORS(app)
+db = SQLAlchemy(app)
 api = Api(app)
+
+# DB Migrate and Upgrades
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # Slack Handler
 class Slack(Resource):
