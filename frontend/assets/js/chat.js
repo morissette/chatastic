92b21230e31dfd6fb75669c353d49176f0f66c99
Matthew Harris <mharris@doublehorn.com>
2016-05-02 14:19:51 -0500

    Initial

---
+++ frontend/assets/js/chat.js
@@ -0,0 +1,122 @@
+/*
+ * Handle scroll fixed from bottom to top
+ */
+$(window).scroll(function () {
+  var mn = $(".nav-login");
+  var viewportHeight = $(window).height();
+
+  if( $(this).scrollTop() > viewportHeight ) {
+    mn.addClass("fixed-to-top");
+  } else {
+    mn.removeClass("fixed-to-top");
+  }
+});
+
+var api_url = 'http://127.0.0.1:5000';
+
+(function() {
+  var app = angular.module('chatastic', ['ngRoute']);
+
+  /*
+   * Handle application frontend routing
+   */
+  app.config(function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/dashboard', {
+          templateUrl: 'partials/dashboard.html',
+          controller: 'dashboardCtrl',
+          currentPage: 'Dashboard'
+        }).
+        when('/pagerduty/settings', {
+          templateUrl: 'partials/pagerduty.html',
+          controller: 'pagerdutyCtrl',
+          currentPage: 'Pagerduty Settings'
+        }).
+        when('/slack/settings', {
+          templateUrl: 'partials/slack.html',
+          controller: 'slackCtrl',
+          currentPage: 'Slack Settings'
+        }).
+        when('/hipchat/settings', {
+       	  templateUrl: 'partials/hipchat.html',
+          controller: 'hipchatCtrl',
+          currentPage: 'HipChat Settings' 
+        }).
+        when('/victorops/settings', {
+		  templateUrl: 'partials/victorops.html',
+          controller: 'victoropsCtrl',
+          currentPage: 'VictorOps Settings'
+        }).
+        when('/opsgenie/settings', {
+		  templateUrl: 'partials/opsgenie.html',
+          controller: 'opsgenieCtrl',
+          currentPage: 'OpsGenie Settings'
+        }).
+        when('/alertops/settings', {
+		  templateUrl: 'partials/alertops.html',
+          controller: 'alertopsCtrl',
+          currentPage: 'AlertOps Settings'
+        }).
+        otherwise({
+          redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+  });
+
+  app.run(function ($rootScope, $route) {
+    $rootScope.$on("$routeChangeSuccess", function(currentRoute, previousRoute) {
+      $rootScope.currentPage = $route.current.currentPage;
+
+      /*
+       * Handle logout all hacky like
+       */
+      if (!$route.current.currentPage) {
+        $rootScope.loggedIn = false;
+      }
+    });
+  });
+
+  /*
+   * Controller Over <body>
+   */
+  app.controller("bodyCtrl", function($scope) {
+    // shouldn't use such a huge controller
+  });
+
+  /*
+   * Handle User Login
+   */
+  app.controller("loginCtrl", function($scope, $rootScope, $location, $http) {
+    var vm = $scope;
+    vm.user = {};
+
+    vm.signIn = function() {
+      var email = vm.user.email;
+      var password = vm.user.password;
+      if ( email && password ) {
+        vm.success = true;
+        $rootScope.loggedIn = true;
+        $location.path('/dashboard');
+      } else {
+        vm.error = "Missing required fields";
+      }
+    };
+
+  });
+
+  /*
+   * Dashboard CTRL
+   */
+  app.controller('dashboardCtrl', function($scope, $rootScope) {
+    $rootScope.loggedIn = true;
+  });
+
+  /*
+   * Settings CTRL
+   */
+  app.controller('settingsCtrl', function($scope) {
+
+  });
+
+
+})();
